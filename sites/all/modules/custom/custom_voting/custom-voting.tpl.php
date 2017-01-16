84bbbf563ab493a6f766201b9c82d8340cacb8d7
Prashant Gaurav <prashu1493@gmail.com>
2017-01-16 15:56:55 +0530

    Creating vote button

---
+++ sites/all/modules/custom/custom_voting/custom-voting.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+print("hello world");
+?>
+
+<html>
+    <head>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/javascript">
+          $(document).ready(function () {<!--  w w  w. j a  v a2  s.  c  om-->
+              $("button").click(function () {
+                  var text = $(this).text();                                
+                  $("input").val(text);
+              });
+          });
+        </script>           
+        </head>
+        <body>
+            <div>
+                <button>Vote</button>                
+            </div>
+            <input type="text" value="click to vote" />
+        </body>
+    </html>
